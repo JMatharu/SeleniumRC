b6e06a538fcdc95a85077a7d98a59a27461a888e
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-05-18 17:59:55 -0400

    Setting up Login Test

--- test/src/com/jmatharu/seleniumRC/TestWebPage.java
+++ test/src/com/jmatharu/seleniumRC/TestWebPage.java
@@ -15,6 +15,7 @@ public class TestWebPage {
 	public void setup() throws InterruptedException {
 		// for IE = "internetexplorer" // for Firefox = "firefox"
 		// Root Portal - http://www.opensourcecms.com/
+		//Portal Details and Credentials - http://www.opensourcecms.com/scripts/details.php?scriptid=88&name=WordPress
 		// Portal to test - http://demo.opensourcecms.com/wordpress/wp-login.php;
 		// in which "http://demo.opensourcecms.com" is domain
 		// and
@@ -27,6 +28,9 @@ public class TestWebPage {
 	@Test
 	public void loginWordPress() {
 		selenium.open("wordpress/wp-login.php");
+		selenium.type("id=user_login", "admin");
+		selenium.type("id=user_pass", "demo123");
+		selenium.click("id=wp-submit");
 	}
 
 	@After
