8357a2d67f674f8c5b3ccb3d4e7f0069fad61534
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-05-18 17:15:06 -0400

    Setting up server for RC

--- test/src/com/jmatharu/seleniumRC/TestWebPage.java
+++ test/src/com/jmatharu/seleniumRC/TestWebPage.java
@@ -1,5 +1,29 @@
 package com.jmatharu.seleniumRC;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.Selenium;
+
 public class TestWebPage {
+
+	@Before
+	public void setup() {
+		
+		Selenium selenium = new DefaultSelenium("localhost", 4444, "*googlechrome*", "http://assignment4emerging.herokuapp.com/login");
+		selenium.start();
+	}
+
+	@Test
+	public void assignment4emerging() {
+
+	}
+
+	@After
+	public void done() {
+
+	}
 
 }
