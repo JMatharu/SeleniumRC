89f9fa7dcda23212feed17b4c3098cf5c598bb69
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-05-18 17:56:21 -0400

    Configuring Domain and Port for TEsting Framework

--- test/src/com/jmatharu/seleniumRC/TestWebPage.java
+++ test/src/com/jmatharu/seleniumRC/TestWebPage.java
@@ -12,18 +12,21 @@ public class TestWebPage {
 	Selenium selenium;
 
 	@Before
-	public void setup() {
+	public void setup() throws InterruptedException {
 		// for IE = "internetexplorer" // for Firefox = "firefox"
 		// Root Portal - http://www.opensourcecms.com/
-		// Portal to test - http://demo.opensourcecms.com/wordpress/wp-login.php
-		selenium = new DefaultSelenium("localhost", 4444, "*googlechrome*",
+		// Portal to test - http://demo.opensourcecms.com/wordpress/wp-login.php;
+		// in which "http://demo.opensourcecms.com" is domain
+		// and
+		// "wordpress/wp-login.php" is Location where I need to go.
+		selenium = new DefaultSelenium("localhost", 4444, "*googlechrome",
 				"http://demo.opensourcecms.com/");
 		selenium.start();
 	}
 
 	@Test
-	public void assignment4emerging() {
-
+	public void loginWordPress() {
+		selenium.open("wordpress/wp-login.php");
 	}
 
 	@After
